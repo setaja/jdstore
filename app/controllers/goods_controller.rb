3c98c00a6deb99e9673e69abec561c5d51c21a98
setaja <setaja@gmail.com>
2016-10-26 19:57:28 +0800

    add new, create, private to controller

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -6,4 +6,23 @@ class GoodsController < ApplicationController
   def index
     @goods = Good.all
   end
+
+  def new
+    @good =Good.new
+  end
+
+  def create
+    @good = Good.new(good_params)
+    if @good.save
+      redirect_to goods_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def good_params
+    params.require(:good).permit(:title, :description)
+  end
+  
 end
