ba29e14400a149ae952a20ba96a951bac3ffcc87
setaja <setaja@gmail.com>
2016-10-28 14:29:25 +0800

    add edit page

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -20,9 +20,23 @@ class GoodsController < ApplicationController
     end
   end
 
+  def edit
+    @good = Good.find(params[:id])
+  end
+
+  def update
+    @good = Good.find(params[:id])
+    if @good.update(good_params)
+      redirect_to goods_path
+    else
+      render :edit
+    end
+  end
+
   private
+
   def good_params
     params.require(:good).permit(:title, :description)
   end
-  
+
 end
