dadee468aa40b11969f4933e9d5a53faaf517831
setaja <setaja@gmail.com>
2016-10-28 15:53:55 +0800

    add admin goods_controller

---
+++ app/controllers/admin/goods_controller.rb
@@ -0,0 +1,50 @@
+class Admin::GoodsController < ApplicationController
+  before_filter :authenticate_user!, only:[:new, :create, :update, :edit, :destroy]
+  def show
+    @good = Good.find(params[:id])
+  end
+
+  def index
+    @goods = Good.all
+  end
+
+  def new
+    @good =Good.new
+  end
+
+  def create
+    @good = Good.new(good_params)
+    if @good.save
+      redirect_to goods_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @good = Good.find(params[:id])
+  end
+
+  def update
+    @good = Good.find(params[:id])
+    if @good.update(good_params)
+      redirect_to goods_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @good = Good.find(params[:id])
+    @good.destroy
+    redirect_to goods_path
+  end
+
+  private
+
+  def good_params
+    params.require(:good).permit(:title, :description)
+  end
+
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :goods
   root 'goods#index'
+  namespace :admin do
+    resources :goods
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
